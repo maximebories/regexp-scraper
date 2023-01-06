2325b8a4342461df2765149fbee27f5227abd6d1
Maxime Bories <30550722+maximebories@users.noreply.github.com>
2023-01-06 04:11:29 +0100

    Major update to match RegExp within result pages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # regex-scraper
-Advanced use of Puppeteer to scrape a web engine results with a RegExp
+Advanced use of Puppeteer to scrape Google web engine results with a RegExp. You have to provide a search query and a regular expression to match against either the Google search results page or the full page content depending on 
 
 ##
 
@@ -12,3 +12,17 @@ To run:
 	$ node index.js
  
 The example I used here was to find fraud phishing URLs send through text messages for further investigations, DO NOT click on any of them unless you know what you are doing.
+
+
+
+## How to use the script
+
+To run the script, use the following command:
+
+node script.js 'search query' 'regular expression' filter
+
+Replace 'search query' with the query you want to use for the search, 'regular expression' with the regular expression you want to use to match against the page content, and 'filter' with 'true' if you want to filter the search results or 'false' if you don't want to filter the results.
+
+For example, to perform a search for the query 'Votre colis a été envoyé. Veuillez le vérifier et le recevoir.' which is a common text phishing in France, disabling filtering Google similar results search, the results and using the regular expression 'http://[a-z]{5}.[a-z]{5}.[a-z]+' that capture all the URLs that are being used in this fishing operation, use the following command:
+
+node script.js 'Votre colis a été envoyé. Veuillez le vérifier et le recevoir.' 'http://[a-z]{5}.[a-z]{5}.[a-z]+' false
--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-// Set query and regex crawling parameters
+// Get search query, regular expression, and "filter" boolean from command line arguments
+const query = process.argv[2].replace(/ /g, '+');
+const regexp = new RegExp(process.argv[3], 'gm');
+const filter = process.argv[4] === 'true';
+
 var data = [];
-const query = '"Votre+colis+a+été+envoyé.+Veuillez+le+vérifier+et+le+recevoir."+"http"+".com"';
-const regexp = /http:\/\/[a-z]{5}\.[a-z]{5}\.[a-z]+/gm;
 
 (async () => {
-	// Init browser
-	const browser = await puppeteer.launch({ headless: false, timeout: 30000 });
+	const browser = await puppeteer.launch({ headless: false, timeout: 5000 });
 	const page = await browser.newPage();
 
-	// Load cookies from previous session
 	const cookiesString = fs.readFileSync('cookies.json');
 	let cookies = JSON.parse(cookiesString);
 	if (cookies.length !== 0) {
@@ -20,35 +20,93 @@ const regexp = /http:\/\/[a-z]{5}\.[a-z]{5}\.[a-z]+/gm;
 		}
 	}
 
-	// Init browsing session & specify we do NOT want to exclude results
-	await page.goto('https://www.google.fr/search?q=' + query + '&filter=0');
+	// Construct Google search URL with search query and "filter" parameter
+	let searchUrl = `https://www.google.fr/search?q=intext%3A%28${query}%29`;
+	if (filter) {
+		searchUrl += '&filter=0';
+	}
+	await page.goto(searchUrl);
 	const navigationPromise = page.waitForNavigation();
 
-	// Loop through pagination & get RegExp matches
 	let loop = true;
+	let iterationCount = 0;
 	while (loop) {
-		const pageContent = await page.content();
-		const urls = pageContent.match(regexp);
-		data.push(urls);
+		// Stop the loop after 20 iterations
+		if (iterationCount >= 20) {
+			loop = false;
+			break;
+		}
+
+		const results = await page.evaluate(() => {
+			const anchors = Array.from(document.querySelectorAll('div[data-header-feature] > div > a'));
+			return anchors.map(anchor => anchor.href);
+		});
+		// Open new tab for each link
+		for (let i = 0; i < results.length; i++) {
+			if (subSearch) {
+				// Open a new tab for each search result and perform sub-search
+				const newPage = await browser.newPage();
+				await newPage.goto(results[i]);
+				// Check if a ReCAPTCHA is present
+				if (await newPage.$('#recaptcha') !== null) {
+					console.log('ReCAPTCHA detected. Pausing loop.');
+					loop = false;
+					break;
+				}
+				// Perform sub-search using the regular expression
+				const subSearchResults = await newPage.evaluate((regexp) => {
+					const pageContent = document.body.innerText;
+					const matches = pageContent.match(regexp);
+					if (matches) {
+						return matches;
+					}
+					return [];
+				}, regexp);
+				if (subSearchResults.length > 0) {
+					data.push(subSearchResults);
+					console.log(subSearchResults);
+				}
+				await newPage.close();
+			} else {
+				// Open a new tab for each search result and get page content
+				const newPage = await browser.newPage();
+				await newPage.goto(results[i]);
+				// Check if a ReCAPTCHA is present
+				if (await newPage.$('#recaptcha') !== null) {
+					console.log('ReCAPTCHA detected. Pausing loop.');
+					loop = false;
+					break;
+				}
+				// Get page content
+				const pageContent = await newPage.content();
+				// Get RegExp matches
+				const matches = pageContent.match(regexp);
+				if (matches) {
+					data.push(matches);
+					console.log(matches);
+				}
+				await newPage.close();
+			}
+		}
+		// Go to next page
 		if (await page.$('#pnnext') !== null) {
+			console.log('Next page');
 			await page.click('#pnnext');
 			await navigationPromise;
 			await page.waitForTimeout(Math.floor(Math.random() * 2000) + 1000);
+			iterationCount++;
 			loop = true;
 		} else {
 			loop = false;
 		}
-	};
+	}
 
-	// Save cookies for later use
 	cookies = await page.cookies();
 	fs.writeFileSync('cookies.json', JSON.stringify(cookies));
 	await browser.close();
 
-	// Save scraped data
 	fs.writeFile("data.json", JSON.stringify(data), err => {
 		if (err) throw err;
 		console.log('Crawling complete.');
-	}
-	);
+	});
 })();
